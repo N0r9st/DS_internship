f203e9c51467ff9ce3afd286a9cbc631fc5a8376
Andrey <62241239+N0r9st@users.noreply.github.com>
2020-07-07 08:23:45 +0300

    Update manage.py

--- deploy/manage.py
+++ deploy/manage.py
@@ -3,7 +3,6 @@
 import os
 import sys
 
-print('MANAGEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE')
 def main():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'deploy.settings')
     try:
